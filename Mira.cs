c2ab1f18b18ea69da7f2267183596d941843c861
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-01-17 19:17:46 -0400

    Commit 1, se agrego 2 nuevas variables, es como un commit de prueba

--- Mira.cs
+++ Mira.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Mira : MonoBehaviour
 {
+    //VARIABLES//
+    //Esto es para medir la sensivilidad de la camara
+    public float Sensitivity = 100f;
+    //Transform es para medir la posicion de un objeto, en esta variable estamos nombrando un objeto como una variable
+    public Transform Player1;
+    //Es para la rotacion en eje X
+    float xRotation = 0f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
